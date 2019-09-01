7d6886ae2f6812c3e67f3deff9f475d2d9770444
abdeen206 <abdeen206@gmail.com>
2019-09-01 17:33:56 +0300

    second commit

--- HW_2_e1.py
+++ HW_2_e1.py
@@ -1,28 +1,70 @@
 #__author__ = Mohammad Abdin
 
+conversation = []
+
 class Human:
-    human_conversation = []
+
     msg=''
     def __init__(self, name):
         self.name = name
         #print("human name is:{}".format(self.name))
     def send(self,msg):
-        self.msg = msg
-        self.human_conversation.append(msg)
-        print(msg)
+        self.msg = self.name +" said: "+ msg
+        conversation.append(self.msg)
+        #print(self.msg)
 
 
 class Robot:
-    robot_conversation = []
+
     msg =''
     def __init__(self, serial_number):
         self.serial_number = serial_number
         #print("robot serial number is:{}".format(self.serial_number))
 
     def send(self,msg):
-        self.msg = msg
-        self.robot_conversation.append(msg)
-        print(msg)
+        self.msg = self.serial_number + " said: " + msg
+        conversation.append(self.msg)
+       # print(self.msg)
+
+
+def show_human_dialogue():
+    for msg in conversation:
+        print(str(msg))
+
+def show_robot_dialogue():
+    for msg in conversation:
+        msg1 = msg.split(":", 1)[0]
+        msg = msg.split(":", 1)[1]
+        for char in msg:
+            #print(char)
+            if char in 'aeiouAEIOU'   :
+                msg = msg.replace(char,'0')
+
+            else:
+                msg = msg.replace(char, '1')
+        print(msg1+": "+ msg)
+
+    # for i in range(len(conversation)):
+    #     for j in range(len(conversation[i])):
+    #         if conversation[i][j] in 'aeiouAEIOU':
+    #             conversation[i] = conversation[i].replace(conversation[i][j],'0')
+    #         else:
+    #             conversation[i] = conversation[i].replace(conversation[i][j],'1')
+    #     print(conversation[i])
+
+# def show_robot_dialogue():
+#     for msg in conversation:
+#         msg1 = msg.split("said: ", 1)[0]
+#         msg2 = msg.split("said: ", 1)[1]
+#         #print(msg2)
+#         for char in msg2:
+#             #print(char)
+#             if char in 'aeiouAEIOU'   :
+#                 msg = msg2.replace(char,'0')
+#
+#             else:
+#                 msg = msg2.replace(char, '1')
+#         print(msg)
 
 
 class Chat:
@@ -41,23 +83,19 @@ class Chat:
         self.robot = robot
         print( "Robot : {} is connected to chat".format(self.robot.serial_number))
 
-    # def show_human_dialogue(self.human):
-    #     return "{} said: {}".format(human.name,input())
-    #
-    # def show_robot_dialogue(self):
-    #     return "{} said: {}".format(robot.serial_number, input())
 
 def main():
     chat = Chat()
-    karl = Human("Karl")
-    bot = Robot("R2D2")
+    karl = Human("my human")
+    bot = Robot("my robot")
     chat.connect_human(karl)
     chat.connect_robot(bot)
 
     karl.send("Hi robot")
     bot.send("hi human")
-    chat.show_human_dialogue()
-    chat.show_robot_dialogue()
+    bot.send("fuck u")
+    show_human_dialogue()
+    show_robot_dialogue()
 
 if __name__ == "__main__":
     main()
